cf908cc5a7e35f8d455407be0070bcbf8df95c7e
K4fle D. Manish <k4fledmanish@aol.com>
2020-12-04 11:50:05 +1100

    Added button to hide and add new list

--- www/js/index.js
+++ www/js/index.js
@@ -11,3 +11,59 @@ for (i = 0; i < myNodelist.length; i++) {
   span.appendChild(txt);
   myNodelist[i].appendChild(span);
 }
+
+// close button to hide
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+// checked symbol
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+// creating a new item
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("Can you please write something!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
+
+function storeToLocal(key, items){
+  localStorage[key] = JSON.stringify(items);
+}
+
+function getFromLocal(key){
+  if(localStorage[key])
+    return JSON.parse(localStorage[key]);
+  else 
+    return [];
+}
